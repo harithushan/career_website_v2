852628f16285a86b0232a16823467dd0520bcc85
Paramanathan Thushanthan <92683441+harithushan@users.noreply.github.com>
2023-05-24 12:41:20 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine, text
 import os
 
 # DB_CONNECTION_STRING
-db_connection_string = os.environ.get('DB_CONNECTION_STRING')
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+# db_connection_string = os.environ.get('DB_CONNECTION_STRING')
 engine = create_engine(
   db_connection_string, 
   connect_args={
